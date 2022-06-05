c29a83b983a6d53bc77103fd952ca7343052d50d
smilecool2012 <smilecool2012@gmail.com>
2022-06-05 14:17:01 +0300

    change pass

--- WebAssistant/WebAssistant/settings.py
+++ WebAssistant/WebAssistant/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'assistant',
         'USER': 'postgres',
-        'PASSWORD': '2012',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
